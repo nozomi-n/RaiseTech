dbc7c62764e3459774106dd786eb5f5dd9da79e7
nozomi.n <hyomabo000@gmail.com>
2020-05-10 18:14:41 +0900

    1~10まで足した結果をfor文で表示する

--- ruby2.rb
+++ ruby2.rb
@@ -1,2 +1,12 @@
+#変数greetingを定義し、HelloWorldの表示
 greeting = "Hello World"
     puts greeting
+
+
+#1~10まで足した結果をfor文で表示
+array = [1,2,3,4,5,6,7,8,9,10]
+sum = 0
+    for i in array 
+        puts sum += i
+    end
+
